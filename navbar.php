6643f6b3579416e6659e4bbde528bfdd0666cb47
Matthias Paulmier <mpaulmier@emi.u-bordeaux1.fr>
2017-03-03 15:08:49 +0100

    Adaptation du code pour le serveur

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<?php
+	$path = "/projet_techno/projet/site/";
+?>
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="<?php echo $path; ?>">Website Test Users</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li class="active"><a href="<?php echo $path; ?>index.php">Home</a></li>
+			<li><a href="<?php echo $path; ?>view/listusers.php">Liste des utilisateurs</a></li>
+			<li><a href="<?php echo $path; ?>view/users.php">Add User</a></li> 
+		</ul>
+	</div>
+</nav>
