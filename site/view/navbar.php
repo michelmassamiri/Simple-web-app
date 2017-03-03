32de2035ea23df2ca9d01b6e0cd1952a3237e582
mmassamire <mmassamire@emi.u-bordeaux1.fr>
2017-03-03 15:23:01 +0100

    modification de la variable path

--- site/view/navbar.php
+++ site/view/navbar.php
@@ -1,5 +1,5 @@
 <?php
-	$path = "/projet_techno/projet/site/";
+	$path = "";
 ?>
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
@@ -9,7 +9,7 @@
 		<ul class="nav navbar-nav">
 			<li class="active"><a href="<?php echo $path; ?>index.php">Home</a></li>
 			<li><a href="<?php echo $path; ?>view/listusers.php">Liste des utilisateurs</a></li>
-			<li><a href="<?php echo $path; ?>view/users.php">Add User</a></li> 
+			<li><a href="<?php echo $path; ?>view/users.php">Add User</a></li>
 		</ul>
 	</div>
 </nav>
