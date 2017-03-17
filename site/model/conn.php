b62e1e1312de612e443f3d7423fe900dbd63e0aa
Michel Massamiri <michel-massamiri@etu.u-bordeaux.fr>
2017-03-17 15:00:51 +0100

    modification pour pouvoir se connecter a la base de donnes

--- site/model/conn.php
+++ site/model/conn.php
@@ -1,23 +1,20 @@
 <?php
-<<<<<<< HEAD
-function connectDB (){   
-    try{
-        $conn = new PDO('mysql:host=' . $this->conf['db_hostname'] . '; dbname=' . $this->conf['db_name'], $this->conf['db_user'], $this->conf['db_password']);
+
+function connectDB ()
+{
+    try {
+        $ini_array = parse_ini_file("config.ini");
+        $server = "localhost";
+        $user = "root";
+        $pass = $ini_array["pass"];
+        $conn = new PDO("mysql:host=localhost;dbname=WEB", $user, $pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }catch(PDOException $err){
-        echo "Connection to DB FAILED " . $err->getMessage();
+    } catch (PDOException $err) {
+        echo "Connection to DB FAILED" . $err->getMessage();
+
     }
     return $conn;
-=======
-try{
-	$ini_array = parse_ini_file("config.ini");
-	$server = "localhost";
-	$user = "root";
-	$pass = $ini_array["pass"];
-	$conn = new PDO("mysql:host=localhost;dbname=WEB",$user,$pass);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $err){
-	echo "Connection to DB FAILED" . $err->getMessage();
->>>>>>> 1d43a2ee58a1401ad6d8f1e99b9cc8eb3de853a5
 }
+
+
 ?>
