7bfc93e96aa37583a14096d93e9101e4a6542587
eperuzzetto <enzoperuzzetto@gmail.com>
2017-04-04 00:10:37 +0200

    add files to make FAQ

--- site/view/navbar.php
+++ site/view/navbar.php
@@ -10,6 +10,7 @@
 			<li class="active"><a href="<?php echo $path; ?>index.php">Home</a></li>
 			<li><a href="<?php echo $path; ?>view/listusers.php">Liste des utilisateurs</a></li>
 			<li><a href="<?php echo $path; ?>view/users.php">Add User</a></li>
+            <li><a href="<?php echo $path; ?>controller/faq.php">FAQ</a></li>
 		</ul>
 	</div>
 </nav>
