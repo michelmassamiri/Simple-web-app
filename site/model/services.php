08dbf7900ce6d4bf9b9ed6b7f2da2492eaddace8
mmassamire <mmassamire@emi.u-bordeaux1.fr>
2017-04-12 12:50:31 +0200

    Ajout des fonctions pour l'admin

--- site/model/services.php
+++ site/model/services.php
@@ -15,4 +15,50 @@ function get_services() {
     return $services;
 
 }
+
+function add_service($categorie, $auteur, $adresse, $date, $lieu, $contenu){
+  require('conn.php');
+
+  $db = connectDB();
+
+  $req = $db->prepare('INSERT INTO PrTec_Services(categorie, auteur, adresse, date, lieu, contenu) VALUES(:categorie, :auteur, :adresse, :date, :lieu, :contenu)');
+  $req->execute(array(
+  	'categorie' => $categorie,
+  	'auteur' => $auteur,
+  	'adresse' => $adresse,
+  	'date' => $date,
+  	'lieu' => $lieu,
+  	'contenu' => $contenu
+  	));
+
+  echo 'Le service a bien été ajouté !';
+
+}
+
+function update_service($data, $id) {
+    require('conn.php');
+
+    $db = connectDB();
+
+    $vars = array();
+    $sql_array = array();
+    foreach ($data as $key => $value) {
+        if(empty($value)) {
+            continue;
+        }
+        else {
+            $vars[] = "{$key} = :nouv_{$key}" ;
+            $sql_array["nouv_{$key}"] = $value;
+        }
+    }
+
+    $vars_imploded = implode("," , $vars);
+    $sql = "UPDATE Services SET {$vars_imploded} WHERE id = {$id}";
+
+    $req = $db->prepare($sql);
+    $req->execute($sql_array);
+
+    return ($req->rowCount() == 1) ? true : false ;
+}
+
 ?>
