52fbb9fb3d50738f40f3473e110cce38da0ff5cf
Matthias Paulmier <mpaulmier@emi.u-bordeaux1.fr>
2017-04-13 16:01:50 +0200

    Modification de la structure

--- admin/index.php
+++ admin/index.php
@@ -25,51 +25,53 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <div class="container">
-        <h1>Installation du projet</h1>
+	<h1>Installation du projet</h1>
         <h2>Bienvenue dans le script d'installation de votre projet</h2><br><br>
-        <form date-toggle="date-validator" class="form-horizontal" action="install.php" method="POST">
-	    <div class="form-group">
-                <h3>Installation de la base de données</h3>
-                <p>
-                    Après avoir terminé de saisir les information demandées sur cette page, un script installera le site sur votre serveur ainsi qu'une base de données.<br>
-                    Le site sera alors opérationnel. 
-                </p>
-                <label class="control-label"> Veuillez saisir le nom de votre projet : </label>
-		<input type="text" name="project_name" placeholder="Nom du projet" class="form-control" required><br>
-                <p>
-                    Ce script installe une base de données sur votre serveur. Cette base de données est nécessaire pour le bon fonctionnement du site.<br>
-                    Cette base de données nécessite les informations suivantes : 
-                </p>
-		<label class="control-label"> Veuillez saisir le nom de la base de données : </label>
-		<input type="text" name="dbname" placeholder="Nom de la base de données" required class="form-control"><br>
-                <p>
-                    Les identifiants de la base de données sont nécessaires pour installer celle-ci.
-                </p>
-                <label class="control-label"> Veuillez saisir le nom de d'utilisateur : </label>
-		<input type="text" name="dbuser" placeholder="Nom d'utilisateur de la base de données" required class="form-control"><br>
-                <label class="control-label"> Veuillez saisir le mot de passe pour cet utilisateur : </label>
-		<input type="text" name="dbpas" placeholder="Mot de passe" required class="form-control"><br>
-                <h3>Création du super utilisateur</h3>
-                <p>
-                    Afin de pouvoir effectuer des actions d'administrations, nous devons créer un compte super utilisateur<br>
-                    Veuillez saisir le nom et le mot de passe de cet utilisateur.
-                </p>
-		<label class="control-label"> Veuillez saisir le nom du super utilisateur : </label>
-		<input type="text" name="suname" placeholder="Nom du super utilisateur" required class="form-control"><br>
-		<label class="control-label"> Veuillez saisir le mot de passe du super utilisateur : </label>
-		<input type="password" name="supass1" placeholder="Mot de passe" required class="form-control"><br>
-		<input type="password" name="supass2" placeholder="Saisir à nouveau" required class="form-control"><br>
-                <h3>Localisation</h3>
-                <p>
-                    Vous allez maintenant pouvoir choisir ou installer votre site.
-                </p>
-		<label class="control-label"> Veuillez saisir le chemin du répertoire d'intallation (chemin actuel par défaut) : </label>
-		<input type="text" name="location" value="<?php echo exec(pwd); ?>" required class="form-control"><br>
-	    </div>
-	    <div class="form-group">
-		<button type="submit" class="btn btn-default">Installation</button>
-            </div>
-        </form>
+        <div class="container">
+            <form date-toggle="date-validator" class="form-horizontal" action="install.php" method="POST">
+		<div class="form-group">
+                    <h3>Installation de la base de données</h3>
+                    <p>
+                        Après avoir terminé de saisir les information demandées sur cette page, un script installera le site sur votre serveur ainsi qu'une base de données.<br>
+                        Le site sera alors opérationnel. 
+                    </p>
+                    <label class="control-label"> Veuillez saisir le nom de votre projet : </label>
+		    <input type="text" name="project_name" placeholder="Nom du projet" class="form-control" required><br>
+                    <p>
+                        Ce script installe une base de données sur votre serveur. Cette base de données est nécessaire pour le bon fonctionnement du site.<br>
+                        Cette base de données nécessite les informations suivantes : 
+                    </p>
+		    <label class="control-label"> Veuillez saisir le nom de la base de données : </label>
+		    <input type="text" name="dbname" placeholder="Nom de la base de données" required class="form-control"><br>
+                    <p>
+                        Les identifiants de la base de données sont nécessaires pour installer celle-ci.
+                    </p>
+                    <label class="control-label"> Veuillez saisir le nom de d'utilisateur : </label>
+		    <input type="text" name="dbuser" placeholder="Nom d'utilisateur de la base de données" required class="form-control"><br>
+                    <label class="control-label"> Veuillez saisir le mot de passe pour cet utilisateur : </label>
+		    <input type="text" name="dbpass" placeholder="Mot de passe" required class="form-control"><br>
+                    <h3>Création du super utilisateur</h3>
+                    <p>
+                        Afin de pouvoir effectuer des actions d'administrations, nous devons créer un compte super utilisateur<br>
+                        Veuillez saisir le nom et le mot de passe de cet utilisateur.
+                    </p>
+		    <label class="control-label"> Veuillez saisir le nom du super utilisateur : </label>
+		    <input type="text" name="suname" placeholder="Nom du super utilisateur" required class="form-control"><br>
+		    <label class="control-label"> Veuillez saisir le mot de passe du super utilisateur : </label>
+		    <input type="password" name="supass1" placeholder="Mot de passe" required class="form-control"><br>
+		    <input type="password" name="supass2" placeholder="Saisir à nouveau" required class="form-control"><br>
+                    <h3>Localisation</h3>
+                    <p>
+                        Vous allez maintenant pouvoir choisir ou installer votre site.
+                    </p>
+		    <label class="control-label"> Veuillez saisir le chemin du répertoire d'intallation (chemin actuel par défaut) : </label>
+		    <input type="text" name="location" value="<?php echo exec(pwd); ?>" required class="form-control"><br>
+		</div>
+		<div class="form-group">
+		    <button type="submit" class="btn btn-default">Installation</button>
+                </div>
+            </form>
+        </div>
     </div>
 </html>
 
